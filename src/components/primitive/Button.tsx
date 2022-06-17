460f8a927b897d80b69e34ee5685cbe3882c598d
infiniwave <felixli101@outlook.com>
2022-06-17 11:06:11 -0500

    fix: button

--- src/components/primitive/Button.tsx
+++ src/components/primitive/Button.tsx
@@ -1,8 +1,8 @@
 import { ComponentChildren, JSX, Ref } from "preact";
 
-const Button = (props: { children: ComponentChildren; onClick?: JSX.MouseEventHandler<HTMLDivElement>, divRef: Ref<HTMLDivElement> }) => {
+const Button = (props: { children: ComponentChildren; onClick?: JSX.MouseEventHandler<HTMLDivElement>, divRef?: Ref<HTMLDivElement>, disabled?: boolean }) => {
     return (
-        <div class="rounded-lg hover:bg-opacity-60 p-2 text-white text-center bg-green-500 hover:bg-green-400 cursor-default select-none" onClick={props.onClick} ref={props.divRef}>
+        <div class={`rounded-lg hover:bg-opacity-60 p-2 text-white text-center cursor-default select-none ${props.disabled ? "bg-gray-400" : "bg-green-500 hover:bg-green-400"}`} onClick={props.disabled ? () => void 0 : props.onClick} ref={props.divRef} disabled={props.disabled}>
             {props.children}
         </div>
     );
