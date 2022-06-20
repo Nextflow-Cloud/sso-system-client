323796e24796b47cbf1c99446038a97f4b5c1402
infiniwave <felixli101@outlook.com>
2022-06-19 21:21:33 -0500

    refactor: forgot wip

--- src/routes/Forgot.tsx
+++ src/routes/Forgot.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars, @typescript-eslint/no-unused-vars */
+// TODO: WIP
 import { useRef, useState } from "preact/hooks";
 import FormBase from "../components/FormBase";
 import Button from "../components/primitive/Button";
@@ -9,31 +11,44 @@ const Forgot = () => {
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
     const [lang, setLang] = useState(localStorage.getItem("lang") || "fr");
+    const [stage, setStage] = useState("initial");
     const next = () => {
-
+        //
     };
-    return (
-        <FormBase loading={loading} setLang={setLang} lang={lang}>
-            <div ref={fade} style={{
-                animation: "1s fadeInRight"
-            }}>
-                <h1 className="text-3xl mb-5"><b>Reset password</b></h1>
-                <div className="inside">  
-                    
-                    <div className='my-1'>
-                        This will walk you through the process of resetting your password.
+    if (stage === "initial") {
+        return (
+            <FormBase loading={loading} setLang={setLang} lang={lang}>
+                <div ref={fade} style={{
+                    animation: "1s fadeInRight"
+                }}>
+                    <h1 className="text-3xl mb-5"><b>Reset password</b></h1>
+                    <div className="inside">  
+                        
+                        <div className='my-1'>
+                            This will walk you through the process of resetting your password.
+                        </div>
+                        <div className="bg-yellow-100 border-yellow-600 border-2 rounded-md my-2 px-2 py-2"><b>WARNING!</b><p>Before you continue, we must remind you that resetting your password will result in irreversable changes! Your encrypted data will be lost as a result of this operation if you have enabled encryption for some services, unless you have a recovery key. Only you (the user) knows the encryption key to your data, not the server, so we cannot assist in recovering the data.</p><br /><p>Only click Next if you know what you are doing.</p></div>
+                        <Button onClick={next} divRef={submit} disabled={loading}>{i18n.translate(lang, "next")}</Button>
                     </div>
-                    <div className="bg-yellow-100 border-yellow-600 border-2 rounded-md my-5 px-2 py-2"><b>WARNING!</b><p>Before you continue, here's a quick reminder that resetting your password will result in irreversable changes! This operation will wipe some of your data, including encrypted files in Titaniumdrive. This is due to the files being encrypted for maximum security using a key derived from your password.</p><br /><p>Only click Next if you know what you are doing.</p></div>
-                    <Button onClick={next} divRef={submit}>{i18n.translate(lang, "next")}</Button>
+                    <div className="inside" />
+                    <p className='inside error'>
+                        {error}
+                    </p>
                 </div>
-                <div className="inside" />
-                <p className='inside error'>
-                    {error}
-                </p>
-            </div>
-        </FormBase>
-    );
+            </FormBase>
+        );
+    }
+    if (stage === "verify") {
+
+    }
+    if (stage === "code") {
+
+    }
+    if (stage === "key") {
+
+    }
+    return <div />;
+
 };
 
-// Please note that your data will be wiped as it is encrypted with a key derived from your old password.
 export default Forgot;
