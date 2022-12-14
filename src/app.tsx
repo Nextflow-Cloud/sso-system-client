43e8abd19d3201c5b0e0614fc6fe763d8e1f46d7
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-12-14 23:36:19 +0000

    refactor: remove unnecessary lazy

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,15 @@
 import { JSX } from "preact";
-import { lazy, Suspense } from "preact/compat";
 import { useEffect, useState } from "preact/hooks";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FormBase from "./components/FormBase";
-import Wave from "./wave";
 
-const Root = lazy(() => import("./routes/Root"));
-const Authenticate = lazy(() => import("./routes/Authenticate"));
-const Login = lazy(() => import("./routes/Login"));
-const Register = lazy(() => import("./routes/Register"));
-const Forgot = lazy(() => import("./routes/Forgot"));
-const Logout = lazy(() => import("./routes/Logout"));
+import FormBase from "./components/FormBase";
+import Authenticate from "./routes/Authenticate";
+import Forgot from "./routes/Forgot";
+import Login from "./routes/Login";
+import Logout from "./routes/Logout";
+import Register from "./routes/Register";
+import Root from "./routes/Root";
+import Wave from "./wave";
 
 const App = () => {
     const [modalDialog, setModalDialog] = useState<JSX.Element>();
@@ -25,16 +24,14 @@ const App = () => {
         <>
             <BrowserRouter>
                 <FormBase loading={loading} lang={lang} setLang={setLang}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Routes>
-                            <Route path="/" element={<Root />} />
-                            <Route path="/authenticate" element={<Authenticate />} />
-                            <Route path="/login" element={<Login loading={loading} setLoading={setLoading} lang={lang} />} />
-                            <Route path="/register" element={<Register loading={loading} setLoading={setLoading} lang={lang} />} />
-                            <Route path="/forgot" element={<Forgot />} />
-                            <Route path="/logout" element={<Logout />} />
-                        </Routes>
-                    </Suspense>
+                    <Routes>
+                        <Route path="/" element={<Root />} />
+                        <Route path="/authenticate" element={<Authenticate />} />
+                        <Route path="/login" element={<Login loading={loading} setLoading={setLoading} lang={lang} />} />
+                        <Route path="/register" element={<Register loading={loading} setLoading={setLoading} lang={lang} />} />
+                        <Route path="/forgot" element={<Forgot />} />
+                        <Route path="/logout" element={<Logout />} />
+                    </Routes>
                 </FormBase>
             </BrowserRouter>
             {modalDialog ? modalDialog : <></>}
