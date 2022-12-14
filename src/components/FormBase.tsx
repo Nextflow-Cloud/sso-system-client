5e24ce556993fe9b9b607d0ad6d72b8615f40036
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-12-14 23:32:13 +0000

    feat: svg logo

--- src/components/FormBase.tsx
+++ src/components/FormBase.tsx
@@ -2,7 +2,7 @@ import { ComponentChildren } from "preact";
 import { StateUpdater, useEffect, useRef, useState } from "preact/hooks";
 import i18n from "../utilities/i18n";
 import Footer from "./Footer";
-import logo from "../logo.png";
+import Logo from "./Logo";
 import styled from "styled-components";
 import SelectMenu from "./primitive/SelectMenu";
 
@@ -65,14 +65,6 @@ const SidePanel = styled.div`
     width: 38%;
 `;
 
-const LogoContainerDesktop = styled.div`
-    margin-bottom: 1.25rem;
-`;
-
-const Logo = styled.img`
-    height: 2rem;
-`;
-
 const MainDesktop = styled.main`
     /* background: linear-gradient(315deg,  0%, rgba(0,119,193,1) 40%, rgba(160,69,210,1) 100%); */
     background: #001F42;
@@ -102,7 +94,8 @@ const DetailsMobile = styled.div`
     margin-bottom: 1.25rem;
 `;
 
-const LogoContainerMobile = styled(LogoContainerDesktop)`
+const LogoContainerMobile = styled.div`
+    margin-bottom: 1.25rem;
     display: flex;
     justify-content: center;
 `;
@@ -134,9 +127,7 @@ const Container = ({ children, loading, lang, setLang }: { children: ComponentCh
                 <FormContainerDesktop> 
                     <SidePanel>
                         <div>
-                            <LogoContainerDesktop>
-                                <Logo src={logo} alt="Nextflow" />
-                            </LogoContainerDesktop>
+                            <Logo />
                             <h2>{i18n.translate(lang, "welcome")}</h2>
                             <p>{i18n.translate(lang, "description")}</p>
                         </div>
@@ -167,48 +158,48 @@ const Container = ({ children, loading, lang, setLang }: { children: ComponentCh
                 <Footer desktop={true} lang={lang} />
             </MainDesktop>
         );
-    } else {
-        return (
-            <MainMobile>
-                <FormContainerMobile>
-                    <GroupMobile>
-                        <DetailsMobile>
-                            <LogoContainerMobile>
-                                <Logo src={logo} alt="Nextflow" />
-                            </LogoContainerMobile>
-                            <h2>{i18n.translate(lang, "welcome")}</h2>
-                            <p>{i18n.translate(lang, "description")}</p>
-                        </DetailsMobile>
-                        <div>
-                            {children}
-                        </div>
-                    </GroupMobile>
-                    <GroupMobile>
-                        <SelectMenu onChange={e => {
-                            setLang((e.target as HTMLSelectElement).value);
-                            localStorage.setItem("lang", (e.target as HTMLSelectElement).value);
-                        }} value={lang}>
-                            <option value="en">{i18n.translate(lang, "english")} (English)</option>
-                                <option value="es">{i18n.translate(lang, "spanish")} (Español)</option>
-                                <option value="fr">{i18n.translate(lang, "french")} (Français)</option>
-                                <option value="de">{i18n.translate(lang, "german")} (Deutsch)</option>
-                                <option value="it">{i18n.translate(lang, "italian")} (Italiano)</option>
-                                <option value="pt">{i18n.translate(lang, "portuguese")} (Português)</option>
-                                <option value="nl">{i18n.translate(lang, "dutch")} (Nederlands)</option>
-                                <option value="pl">{i18n.translate(lang, "polish")} (Polski)</option>
-                                <option value="ru">{i18n.translate(lang, "russian")} (Русский)</option>
-                                <option value="uk">{i18n.translate(lang, "ukrainian")} (Українська)</option>
-                                <option value="zh">{i18n.translate(lang, "chinese")} (中文)</option>
-                                <option value="ja">{i18n.translate(lang, "japanese")} (日本語)</option>
-                                <option value="ko">{i18n.translate(lang, "korean")} (한국어)</option>
-                                <option value="vi">{i18n.translate(lang, "vietnamese")} (Tiếng Việt)</option>
-                        </SelectMenu>
-                    </GroupMobile>
-                </FormContainerMobile>
-                <Footer desktop={false} lang={lang} />
-            </MainMobile>
-        );
-    }
+    } 
+    return (
+        <MainMobile>
+            <FormContainerMobile>
+                <GroupMobile>
+                    <DetailsMobile>
+                        <LogoContainerMobile>
+                            <Logo />
+                        </LogoContainerMobile>
+                        <h2>{i18n.translate(lang, "welcome")}</h2>
+                        <p>{i18n.translate(lang, "description")}</p>
+                    </DetailsMobile>
+                    <div>
+                        {children}
+                    </div>
+                </GroupMobile>
+                <GroupMobile>
+                    <SelectMenu onChange={e => {
+                        setLang((e.target as HTMLSelectElement).value);
+                        localStorage.setItem("lang", (e.target as HTMLSelectElement).value);
+                    }} value={lang}>
+                        <option value="en">{i18n.translate(lang, "english")} (English)</option>
+                        <option value="es">{i18n.translate(lang, "spanish")} (Español)</option>
+                        <option value="fr">{i18n.translate(lang, "french")} (Français)</option>
+                        <option value="de">{i18n.translate(lang, "german")} (Deutsch)</option>
+                        <option value="it">{i18n.translate(lang, "italian")} (Italiano)</option>
+                        <option value="pt">{i18n.translate(lang, "portuguese")} (Português)</option>
+                        <option value="nl">{i18n.translate(lang, "dutch")} (Nederlands)</option>
+                        <option value="pl">{i18n.translate(lang, "polish")} (Polski)</option>
+                        <option value="ru">{i18n.translate(lang, "russian")} (Русский)</option>
+                        <option value="uk">{i18n.translate(lang, "ukrainian")} (Українська)</option>
+                        <option value="zh">{i18n.translate(lang, "chinese")} (中文)</option>
+                        <option value="ja">{i18n.translate(lang, "japanese")} (日本語)</option>
+                        <option value="ko">{i18n.translate(lang, "korean")} (한국어)</option>
+                        <option value="vi">{i18n.translate(lang, "vietnamese")} (Tiếng Việt)</option>
+                    </SelectMenu>
+                </GroupMobile>
+            </FormContainerMobile>
+            <Footer desktop={false} lang={lang} />
+        </MainMobile>
+    );
+    
 };
 
 export default Container;
