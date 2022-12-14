b46fde858c6aff87798f64321f161d1f4e2fd4c6
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-12-14 23:34:05 +0000

    style: remove stray comment

--- src/components/ErrorText.tsx
+++ src/components/ErrorText.tsx
@@ -2,9 +2,6 @@ import styled from "styled-components";
 
 const ErrorText = styled.p`
     color: #f64f59;
-    /* &:empty::before {
-        content: "\\200b\\ssss\\200b";
-    } */
     min-height: 48px;
 `;
 
