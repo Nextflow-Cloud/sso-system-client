0844846f98c5722b97791d7d5f67ab7ed7701cfa
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2023-01-15 21:03:11 +0000

    fix: forgot dialog

--- src/routes/Forgot.tsx
+++ src/routes/Forgot.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable no-unused-vars, @typescript-eslint/no-unused-vars */
 // TODO: WIP
 import { useRef, useState } from "preact/hooks";
-import FormBase from "../components/FormBase";
+import ErrorText from "../components/ErrorText";
+import Fade from "../components/Fade";
+import Box from "../components/primitive/Box";
 import Button from "../components/primitive/Button";
+import Title from "../components/primitive/Title";
 import i18n from "../utilities/i18n";
 
 const Forgot = () => {
@@ -11,7 +14,7 @@ const Forgot = () => {
     const [lang, setLang] = useState(localStorage.getItem("lang") || "fr");
     const [stage, setStage] = useState("initial");
     
-    const fade = useRef<HTMLDivElement>(null);
+    const [hiding, setHiding] = useState(false);
     const submit = useRef<HTMLDivElement>(null);
 
     const next = () => {
@@ -20,24 +23,23 @@ const Forgot = () => {
 
     if (stage === "initial") {
         return (
-            <FormBase loading={loading} setLang={setLang} lang={lang}>
-                <div ref={fade} style={{
-                    animation: "1s fadeInRight"
-                }}>
-                    <h1 class="text-3xl mb-5"><b>Reset password</b></h1>
-                    <div class="inside">  
-                        <div class="my-1">
-                            This page will walk you through the process of resetting your password.
-                        </div>
-                        <div class="bg-yellow-100 border-yellow-600 border-2 rounded-md my-2 px-2 py-2"><b>WARNING!</b><p>Before you continue, we must remind you that resetting your password will result in irreversable changes! If encryption is enabled for any services, encrypted data in those services will be lost as a result of this operation unless you have a recovery key. Only you know the encryption key to your data, and not us. This grants you maximum privacy, however, we cannot assist in recovering your data.</p><br /><p>Only click Next if you are sure of what you are doing.</p></div>
-                        <Button onClick={next} disabled={loading}>{i18n.translate(lang, "next")}</Button>
+            <Fade hiding={hiding}>
+                <Title>Reset password</Title>
+                <div>  
+                    <div class="my-1">
+                        This page will walk you through the process of resetting your password.
                     </div>
-                    <div class="inside" />
-                    <p class="inside error">
-                        {error}
-                    </p>
+                    <Box type="information">
+                        <b>WARNING!</b>
+                        <p>Before you continue, we must remind you that resetting your password will result in irreversable changes! If encryption is enabled for any services, encrypted data in those services will be lost as a result of this operation unless you have a recovery key. Only you know the encryption key to your data, and not us. This grants you maximum privacy, however, we cannot assist in recovering your data.</p><br /><p>Only click Next if you are sure of what you are doing.</p>
+                    </Box>
+                    <Button onClick={next} disabled={loading}>{i18n.translate(lang, "next")}</Button>
                 </div>
-            </FormBase>
+                <div class="inside" />
+                <ErrorText>
+                    {error}
+                </ErrorText>
+            </Fade>
         );
     }
     if (stage === "verify") {
