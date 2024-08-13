a49491642c6b0396f15af0f2fb2c87fc64f8bca0
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2024-08-13 18:32:12 +0000

    fix: box colours

--- src/components/primitive/Box.tsx
+++ src/components/primitive/Box.tsx
@@ -6,9 +6,8 @@ const SuccessBox = styled.div`
     /* background-color: rgb(20 83 45);
     border-color: rgb(74 222 128); */
     background-color: #86efac;
-    border-color: #22c55e;
+    border-color: #22c55e 2px solid;
     border-radius: 0.375rem;
-    border-width: 2px;
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
     padding: 0.5rem;
@@ -18,9 +17,30 @@ const InformationBox = styled.div`
     /* background-color: rgb(30 58 138); */
     /* border-color: rgb(96 165 250); */
     background-color: #0ea5e9;
-    border-color: #7dd3fc;
+    border: #7dd3fc 2px solid;
     border-radius: 0.375rem;
-    border-width: 2px;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem;
+`;
+
+const ErrorBox = styled.div`
+    /* background-color: rgb(139 0 0);
+    border-color: rgb(255 0 0); */
+    background-color: #ff7b7b;
+    border: #ff0000 2px solid;
+    border-radius: 0.375rem;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem;
+`;
+
+const WarningBox = styled.div`
+    /* background-color: rgb(138 138 0);
+    border-color: rgb(255 255 0); */
+    background-color: #f5f5a5;
+    border: #ffff00 2px solid;
+    border-radius: 0.375rem;
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
     padding: 0.5rem;
@@ -40,7 +60,21 @@ const Box = (props: ParentProps<{ type: "success" | "error" | "warning" | "infor
                 {props.children}
             </InformationBox>
         );
-    } 
+    } else
+    if (props.type === "error") {
+        return (
+            <ErrorBox>
+                {props.children}
+            </ErrorBox>
+        );
+    } else
+    if (props.type === "warning") {
+        return (
+            <WarningBox>
+                {props.children}
+            </WarningBox>
+        );
+    }
     return (
         <div>
             {props.children}
