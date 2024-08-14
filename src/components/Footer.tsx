a40aaa737c05e86c3b2fa902cdcf8f3ad8c87c0c
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2024-08-14 23:07:10 +0000

    refactor: extract version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import { Accessor } from "solid-js";
 import { styled } from "solid-styled-components";
 import { Language, translate } from "../utilities/i18n";
 import Link from "./primitive/Link";
+import { VERSION } from "../constants";
 
 
 const FooterBase = styled.footer`
@@ -16,7 +17,7 @@ const Footer = ({ desktop, lang }: { desktop: boolean; lang: Accessor<Language>;
     return (
         <FooterBase desktop={desktop}>
             <p>{translate(lang(), "SSO_SYSTEM")}</p>
-            <p>{translate(lang(), "SSO_SYSTEM_VERSION").replace("{}", "1.0.1")}</p>
+            <p>{translate(lang(), "SSO_SYSTEM_VERSION").replace("{}", VERSION)}</p>
             <p>{translate(lang(), "SSO_SYSTEM_COPYRIGHT").replace("{}", new Date().getUTCFullYear().toString())}</p>
             <p><Link href="https://nextflow.cloud/terms">{translate(lang(), "TERMS")}</Link> | <Link href="https://nextflow.cloud/privacy">{translate(lang(), "PRIVACY")}</Link></p>
         </FooterBase>
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const TRUSTED_SERVICES: string[] = import.meta.env.NEXTFLOW_TRUSTED_SERVICES.split(",");
 export const CAPTCHA_KEY: string = import.meta.env.NEXTFLOW_CAPTCHA_KEY;
+export const VERSION: string = "1.0.1"; 
